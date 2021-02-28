1c6c997670df58eea6465bcd892c1b7cc0e2a1cf
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-02-28 04:30:15 +0300

    SaAs feature added

--- CumaBot.Function/Immino.cs
+++ CumaBot.Function/Immino.cs
@@ -10,21 +10,45 @@ namespace CumaBot.Function
     public static class Immino
     {
         private static DiscordSocketClient _client;
-        
+
+        [FunctionName("GoodMorning")]
+        public static async Task SaAs([TimerTrigger("0 0 */6 * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = "ODEyMzE1OTM5NTA4OTc3NjY0.YC--LA.0TddcTf2EamPSBi-FmbjyVpybzQ";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.Ready += async () =>
+            {
+                SocketGuild socketGuild = _client.GetGuild(764931336191868939);
+                SocketTextChannel socketTextChannel = socketGuild.GetTextChannel(804687744890699796);
+                await socketTextChannel.SendMessageAsync($"Sa As #{DateTime.Now.Hour - DateTime.Now.Minute}");
+                log.LogInformation("Sa As Message Sent!");
+            };
+        }
+
+
         [FunctionName("Immino")]
         public static async Task RunAsync([TimerTrigger("0 0 8 * * Fri")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
-            
+
             _client = new DiscordSocketClient();
-            
+
             _client.Log += Log;
-            
+
             var token = "ODEyMzE1OTM5NTA4OTc3NjY0.YC--LA.0TddcTf2EamPSBi-FmbjyVpybzQ";
-            
+
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
-            
+
             _client.Ready += () => ClientOnReady(log);
         }
 
